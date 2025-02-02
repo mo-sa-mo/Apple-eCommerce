8c3e3a92ae8cefbf7b4b56878711cb4eee85a5f6
Nicolas <mohamedsabirr2000@gmail.com>
2025-02-02 17:34:33 +0300

    All Sections Is Complete

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// Common Js
-document.querySelectorAll(".watch-control, .controls a").forEach((control) => {
-  control.addEventListener("click", (e) => {
-    e.preventDefault();
+// Common JS
+document
+  .querySelectorAll(".watch-control, .controls a, .iphone-btn")
+  .forEach((control) => {
+    control.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
   });
-});
-// End Of Common Js
+// End of Common JS
 
 // Cube
 let x = 0;
@@ -18,20 +20,25 @@ const cube = document.querySelector(".cube");
 document.querySelector(".top-x-control").addEventListener("click", () => {
   cube.style.transform = `rotateX(${(x += 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
 });
+
 document.querySelector(".bottom-x-control").addEventListener("click", () => {
   cube.style.transform = `rotateX(${(x -= 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
 });
+
 document.querySelector(".left-y-control").addEventListener("click", () => {
-  cube.style.transform = `rotateX(${x}deg) rotateY(${(y -= 20)}deg) rotateZ(${z}deg)`;
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y -= 20)}deg) rotateZ(${z}deg) `;
 });
+
 document.querySelector(".right-y-control").addEventListener("click", () => {
-  cube.style.transform = `rotateX(${x}deg) rotateY(${(y += 20)}deg) rotateZ(${z}deg)`;
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y += 20)}deg) rotateZ(${z}deg) `;
 });
+
 document.querySelector(".top-z-control").addEventListener("click", () => {
-  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z -= 20)}deg)`;
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z -= 20)}deg) `;
 });
+
 document.querySelector(".bottom-z-control").addEventListener("click", () => {
-  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z += 20)}deg)`;
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z += 20)}deg) `;
 });
 
 const playPause = () => {
@@ -43,36 +50,48 @@ const playPause = () => {
     clearInterval(interval);
   }
 };
+
 playPause();
+
 document.querySelector(".controls").addEventListener("mouseover", () => {
   bool = false;
   playPause();
 });
+
 document.querySelector(".controls").addEventListener("mouseout", () => {
   bool = true;
   playPause();
 });
-// End Of Cube
 
-// SlideShow
-const slideShowDivs = () => {
+// End of Cube
+
+// Slideshow
+const slideshowDivs = () => {
   for (let i = 1; i <= 5; i++) {
     const div = document.createElement("div");
 
-    div.style.backgroundImage = `url("./images/slideshow/section-1-bg-${i}.jpg")`;
-    1 === i && div.classList.add("change");
+    div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`;
+
+    i === 1 && div.classList.add("change");
+
     document.querySelector(".slideshow").appendChild(div);
   }
 };
-slideShowDivs();
+
+slideshowDivs();
+
 const divs = document.querySelectorAll(".slideshow div");
+
 let a = 1;
+
 const slideshow = () => {
   setInterval(() => {
     a++;
 
     const div = document.querySelector(".slideshow .change");
+
     div.classList.remove("change");
+
     if (a < divs.length) {
       div.nextElementSibling.classList.add("change");
     } else {
@@ -81,11 +100,13 @@ const slideshow = () => {
     }
   }, 20000);
 };
+
 slideshow();
-// End Of SlideShow
+// End of Slideshow
 
 // Section 3
 const section3Content = document.querySelector(".section-3-content");
+
 window.addEventListener("scroll", () => {
   if (
     window.pageYOffset + window.innerHeight >=
@@ -94,7 +115,7 @@ window.addEventListener("scroll", () => {
     section3Content.classList.add("change");
   }
 });
-// End Of Section 3
+// End of Section 3
 
 // Section 4
 const watchBands = document.querySelector(".watch-bands");
@@ -107,23 +128,27 @@ const watchLeftControl = document.querySelector(".watch-left-control");
 
 let axisY = 0;
 let axisX = 0;
+
 const hideControl = () => {
   if (axisY === -280) {
     watchTopControl.classList.add("hideControl");
   } else {
     watchTopControl.classList.remove("hideControl");
   }
+
   if (axisY === 280) {
     watchBottomControl.classList.add("hideControl");
   } else {
     watchBottomControl.classList.remove("hideControl");
   }
-  if (axisX === -280) {
+
+  if (axisX === 280) {
     watchRightControl.classList.add("hideControl");
   } else {
     watchRightControl.classList.remove("hideControl");
   }
-  if (axisX === 280) {
+
+  if (axisX === -280) {
     watchLeftControl.classList.add("hideControl");
   } else {
     watchLeftControl.classList.remove("hideControl");
@@ -133,18 +158,20 @@ const hideControl = () => {
 watchTopControl.addEventListener("click", () => {
   watchCases.style.marginTop = `${(axisY -= 70)}rem`;
   hideControl();
-  console.log(axisY);
 });
+
 watchBottomControl.addEventListener("click", () => {
   watchCases.style.marginTop = `${(axisY += 70)}rem`;
   hideControl();
 });
+
 watchRightControl.addEventListener("click", () => {
-  watchBands.style.marginLeft = `${(axisX -= 70)}rem`;
+  watchBands.style.marginRight = `${(axisX += 70)}rem`;
   hideControl();
 });
+
 watchLeftControl.addEventListener("click", () => {
-  watchBands.style.marginLeft = `${(axisX += 70)}rem`;
+  watchBands.style.marginRight = `${(axisX -= 70)}rem`;
   hideControl();
 });
-// End Of Section 4
+// End of Section 4
